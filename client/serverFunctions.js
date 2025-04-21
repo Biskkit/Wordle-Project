76c15b166fb8055ffbd07147e3162870ba4ee63e
Biskkit <abdulbasitsalim921@gmail.com>
2025-04-21 13:56:22 -0400

    Fix server_url -> server_ip

--- client/serverFunctions.js
+++ client/serverFunctions.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 // Server IP
-const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+const SERVER_IP = process.env.REACT_APP_SERVER_URL;
 
 // Simply grabs the word of the day
 export async function getWordOfDay() {
