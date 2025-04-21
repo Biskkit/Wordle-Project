449e7b2fdfc3eb651639bcb61512bac24187fd87
Biskkit <abdulbasitsalim921@gmail.com>
2025-04-21 10:42:16 -0400

    Move constants to separate file

---
+++ client/src/consts.js
@@ -0,0 +1,6 @@
+/* File to store constants I need */
+export const MAX_LEN = 5;
+export const MAX_GUESSES = 6;
+export const GRAY = "rgb(58, 58, 60)";
+export const YELLOW = "rgb(181, 159, 59)";
+export const GREEN = "rgb(83, 141, 78)";
