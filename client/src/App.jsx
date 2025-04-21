aafcf1baab73fb8b47351b9d95306990ee8d2068
Biskkit <abdulbasitsalim921@gmail.com>
2025-04-21 06:29:53 -0400

    Implement updating keyboard color

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,45 @@
 import './App.css'
 import { InputArea } from './components/inputArea'
 function App() {
+  // Current correct word to match guess with
+  // (Just a placeholder for now)
+  let [correctWord, setCorrectWord] = useState("chips");
+  // Array of guesses
+  let [guesses, setGuesses] = useState(['', '', '', '', '', '']);
+  // Array of colors for each guess
+  let [guessColors, setGuessColors] = useState([[], [], [], [], [], []]);
+  // Current index into guesses array
+  let [curIndex, index] = useState(0);
+  // Store mapping of each letter to the corresponding color
+  // It'll be mapped via the character code ("Z" - "A" = 25)
+  let [keyboardColors, setKeyboardColors] = Array(26).fill("gray");
+  /**
+   * Updates keyboard colors by grabbing the current word and the corresponding color array
+   */
+  function updateKeyboardColors() {
+    // Grab current guess
+    const guess = guesses[curIndex];
+    // Grab corresponding colors
+    const colors = guessColors[curIndex];
+    // Create new keyboard colors array
+    const updatedKbColors = [...keyboardColors];
+    
+    // Iterate through, updating if needed
+    for(let i = 0; i < 5; i++) {
+      // grab corresponding color
+      const color = colors[i];
+      // Grab index, mapping letter to its position in alphabet
+      const index = guess.charCodeAt(i) - 'A'.charCodeAt(0);
+
+      // Now, check if keyboard color is green, if so, there's no need to update, so just continue
+      if(keyboardColors[index] == 'green') continue;
+      // Next check if color is yellow or green, if so update
+      if(color == 'yellow' || color == 'green') updatedKbColors[index] = color;
+    }
+    // update state
+    setKeyboardColors(updatedKbColors);
+  }
+
   return (
     <>
       WORDLE
