a62c2bbb2a1161796af7487e3c58ffa6e4fb1419
Biskkit <abdulbasitsalim921@gmail.com>
2025-04-21 12:21:45 -0400

    Implement callback for event. Now it listens without needing to call e.focus()

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import { useEffect, useState, useRef, createContext } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { InputArea } from './components/inputArea'
 import { MAX_LEN, GRAY, YELLOW, GREEN, MAX_GUESSES} from './consts';
 import { OnScreenKeyBoard } from './components/screenKeyboard';
 
 function App() {
   // Ref for focusing on main div
-  let mainDiv = useRef(null);
+  // let mainDiv = useRef(null);
   // State to store if the correct guess has been made.
   // When this happens, no further input should be accepted.
   // Note: Input can also be halted when the curIndex reaches 5, i.e. when the max guesses have been reached
@@ -25,99 +25,107 @@ function App() {
   // It'll be mapped via the character code ("Z" - "A" = 25)
   let [keyboardColors, setKeyboardColors] = useState(Array(26).fill(''));
 
-  // Context for passing down necessary functions to components
-  // Focuses on div
-  useEffect(() => {
-    if(mainDiv.current) mainDiv.current.focus();
-  }, [mainDiv])
 
-  /**
+/**
    * Adds extra word to the end of curString, should the length of curString be less than 5
    * @param {KeyboardEvent} e, keyevent 
    */
-  function handleKeyPress(key) {
-    // Check for edge cases where either 1. The user guessed correctly already or 2. They reached max number of guesses at 6
-    if(curIndex === MAX_GUESSES || done) return;
-    
-    const curString = guesses[curIndex];
-    const curKey = key.toUpperCase();
-    if(curString.length < MAX_LEN && key.length == 1 && (curKey.charCodeAt(0)) >= "A".charCodeAt(0) && (curKey.charCodeAt(0)) <= 'Z'.charCodeAt(0)) {
-      // Update guesses array to reflect this string
-      const newGuesses = [...guesses];
-      newGuesses[curIndex] = newGuesses[curIndex] + curKey;
-      setGuesses(newGuesses);
-    }
-    // else, check if it's the enter key and handle accordingly. validating that the current guess is valid
-    // and then initializing the color array at the current index
-    else if(key == "Enter") {
-      if(curString.length == MAX_LEN) {
-        // Since the word is filled out, update the guessColors array at the current index.
-        // And update the keyboard colors array as well
+const handleKeyPress = useCallback( (key) => {
+  // Check for edge cases where either 1. The user guessed correctly already or 2. They reached max number of guesses at 6
+  if(curIndex === MAX_GUESSES || done) return;
+  
+  const curString = guesses[curIndex];
+  const curKey = key.toUpperCase();
+  if(curString.length < MAX_LEN && key.length == 1 && (curKey.charCodeAt(0)) >= "A".charCodeAt(0) && (curKey.charCodeAt(0)) <= 'Z'.charCodeAt(0)) {
+    // Update guesses array to reflect this string
+    const newGuesses = [...guesses];
+    newGuesses[curIndex] = newGuesses[curIndex] + curKey;
+    setGuesses(newGuesses);
+  }
+  // else, check if it's the enter key and handle accordingly. validating that the current guess is valid
+  // and then initializing the color array at the current index
+  else if(key == "Enter") {
+    if(curString.length == MAX_LEN) {
+      // Since the word is filled out, update the guessColors array at the current index.
+      // And update the keyboard colors array as well
 
-        // Grab colors array
-        const colors = getColors(curString, correctWord);
-        // Copy guessColors, and then update state
-        const updatedColors = [...guessColors];
-        updatedColors[curIndex] = colors;
-        setGuessColors(updatedColors);
+      // Grab colors array
+      const colors = getColors(curString, correctWord);
+      // Copy guessColors, and then update state
+      const updatedColors = [...guessColors];
+      updatedColors[curIndex] = colors;
+      setGuessColors(updatedColors);
 
-        // update keyboard colors
-        updateKeyboardColors(updatedColors);
-        
-        // Check if curString == correctWord. If so, set done to true
-        if(curString == correctWord) {
-          setDone(true);
-        }
-        // increment index
-        setCurIndex(curIndex+1);
+      // update keyboard colors
+      updateKeyboardColors(updatedColors);
+      
+      // Check if curString == correctWord. If so, set done to true
+      if(curString == correctWord) {
+        setDone(true);
       }
-    }
-    // Finally, check if backspace was hit, in which case, the last letter inputted shall be deleted
-    // Edge case: Word is empty 
-    // No need to check MAX_LEN or anything since we're not adding anything
-    else if(key == "Backspace") {
-      // Return if string is already empty, don't need to do anything
-      if(curString.length == 0) return;
-
-      // Otherwise, delete the last letter of the string
-      const newGuesses = [...guesses];
-      newGuesses[curIndex] = curString.substring(0, curString.length - 1);
-      // Update state
-      setGuesses(newGuesses);
+      // increment index
+      setCurIndex(curIndex+1);
     }
   }
+  // Finally, check if backspace was hit, in which case, the last letter inputted shall be deleted
+  // Edge case: Word is empty 
+  // No need to check MAX_LEN or anything since we're not adding anything
+  else if(key == "Backspace") {
+    // Return if string is already empty, don't need to do anything
+    if(curString.length == 0) return;
 
-  /**
-   * Updates keyboard colors by grabbing the current word and the corresponding color array
-   */
-  function updateKeyboardColors(colors) {
-    // Grab current guess
-    const guess = guesses[curIndex];
-    // Grab corresponding colors
-    const curRowColors = colors[curIndex];
-    // Create new keyboard colors array
-    const updatedKbColors = [...keyboardColors];
-      console.log("Colors: ", colors);
-    // Iterate through, updating if needed
-    for(let i = 0; i < 5; i++) {
-      // grab corresponding color
-      const color = curRowColors[i];
-      // Grab index, mapping letter to its position in alphabet
-      const index = guess.charCodeAt(i) - 'A'.charCodeAt(0);
-
-      // Now, check if keyboard color is green, if so, there's no need to update, so just continue
-      if(keyboardColors[index] == GREEN) continue;
-      // Next check if color is yellow or green, if so update
-      if(color == YELLOW || color == GREEN) updatedKbColors[index] = color;
-      // Finally, check if the color is gray. Then it should only update when the keyboard hasn't stored a color there (i.e. the empty string)
-      else  if(color == GRAY && updatedKbColors[index].length == 0) updatedKbColors[index] = color;
-    }
-    // update state
-    setKeyboardColors(updatedKbColors);
+    // Otherwise, delete the last letter of the string
+    const newGuesses = [...guesses];
+    newGuesses[curIndex] = curString.substring(0, curString.length - 1);
+    // Update state
+    setGuesses(newGuesses);
   }
+
+   /**
+ * Updates keyboard colors by grabbing the current word and the corresponding color array
+ */
+function updateKeyboardColors(colors) {
+  // Grab current guess
+  const guess = guesses[curIndex];
+  // Grab corresponding colors
+  const curRowColors = colors[curIndex];
+  // Create new keyboard colors array
+  const updatedKbColors = [...keyboardColors];
+    console.log("Colors: ", colors);
+  // Iterate through, updating if needed
+  for(let i = 0; i < 5; i++) {
+    // grab corresponding color
+    const color = curRowColors[i];
+    // Grab index, mapping letter to its position in alphabet
+    const index = guess.charCodeAt(i) - 'A'.charCodeAt(0);
+
+    // Now, check if keyboard color is green, if so, there's no need to update, so just continue
+    if(keyboardColors[index] == GREEN) continue;
+    // Next check if color is yellow or green, if so update
+    if(color == YELLOW || color == GREEN) updatedKbColors[index] = color;
+    // Finally, check if the color is gray. Then it should only update when the keyboard hasn't stored a color there (i.e. the empty string)
+    else  if(color == GRAY && updatedKbColors[index].length == 0) updatedKbColors[index] = color;
+  }
+  // update state
+  setKeyboardColors(updatedKbColors);
+}
+}, [guesses, curIndex, guessColors, done, correctWord, keyboardColors]);
+
+
+  // Context for passing down necessary functions to components
+  // Focuses on div
+  useEffect(() => {
+    const listener = (e) => handleKeyPress(e.key);
+    window.addEventListener("keydown", listener);
+    return () => window.removeEventListener("keydown", listener)
+  }, [handleKeyPress])
+
+  
+
+ 
 
   return (
-    <div className='main' ref={mainDiv} tabIndex={0} onKeyDown={(e) => handleKeyPress(e.key)}>
+    <div className='main' tabIndex={0} onKeyDown={(e) => handleKeyPress(e.key)}>
       WORDLE
       <InputArea guesses={guesses} colors={guessColors}/>
       <OnScreenKeyBoard keyboardColors={keyboardColors} handleKeyPress={handleKeyPress}/>
